0924ab076b6b1b08605a4ed3f7b7e2f3897673f5
Laiba <sajidrubab36@gmail.com>
2024-05-11 10:40:52 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+console.log("Welcome to Guess the Number Game");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+let number = await inquirer.prompt([
+    {
+        name: "userNumber",
+        type: "number",
+        message: "Enter your number from( 1 to 5):",
+    },
+]);
+if (number.userNumber == randomNumber) {
+    console.log("Congratulation! You Won");
+}
+else {
+    console.log("Try Again");
+}
